626c51a3734c3b6ff44d87d3f131d05252793fdf
Nguyhithatgud <nguyh432@gmail.com>
2025-11-22 11:11:56 +0700

    Add setIsFlipped function to flip back to login after signup

--- mora-community/src/stores/useLoginformStore.js
+++ mora-community/src/stores/useLoginformStore.js
@@ -7,6 +7,7 @@ export const useLoginformStore = create((set) => ({
     isFlipped: false,
     setSignUpAnim: (value) => set({ signUpAnim: value }),
     setSignInAnim: (value) => set({ signInAnim: value }),
+    setIsFlipped: (value) => set({ isFlipped: value }),
     toggleFlip: () => set((state) => ({ isFlipped: !state.isFlipped })),
     login: () => set({ isLoggedIn: true }),
     logout: () => set({ isLoggedIn: false }),
